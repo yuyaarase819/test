aebf30601d732ae562b6301af4d52644eb0566d4
Tatsuyoshi Mizuno <tatsuyoshi964@gmail.com>
2016-10-30 14:33:25 +0900

    watchへpugを追加

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,6 +3,7 @@ var config = require('../config');
 
 gulp.task('watch', function(){
     gulp.watch(config.src + '/scss/*.scss', ['sass']);
+    gulp.watch(config.src + '/pug/*.pug', ['pug']);
     gulp.watch(config.src + '/js/*.js', ['webpack']);
     gulp.watch(config.src + '/js/*.jsx', ['webpack']);
 });
