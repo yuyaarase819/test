7e9f9370543ecf86e9f6a0a4b5ee135fe6280654
Tatsuyoshi Mizuno <tatsuyoshi964@gmail.com>
2016-11-19 17:17:53 +0900

    SASSの開発環境を修正

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var cssnext = require('postcss-cssnext');
 var config = require('../config');
 
 gulp.task('sass', function() {
+  var processors = [
+    cssnext()
+  ];
   gulp.src(config.entryFiles.css)
     .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(postcss(processors))
     .pipe(gulp.dest(config.destDirs.css));
 });
