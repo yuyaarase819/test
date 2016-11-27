4c5f9c12166bf214f81fb584d6fd97d56355156f
Tatsuyoshi Mizuno <tatsuyoshi964@gmail.com>
2016-11-27 19:48:51 +0900

    pugテンプレート構造の構築

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,5 +4,7 @@ var config = require('../config');
 gulp.task('watch', function(){
     gulp.watch(config.src + '/scss/*.scss', ['sass']);
     gulp.watch(config.src + '/pug/*.pug', ['pug']);
+    gulp.watch(config.src + '/pug/**/*.pug', ['pug']);
+    gulp.watch(config.src + '/pug/**/**/*.pug', ['pug']);
     gulp.watch(config.src + '/js/*.js', ['browserify']);
 });
